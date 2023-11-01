1491a06e74214a1beea9ccccd1a794678c471009
n0n3nt1ty6857 <1043954252@qq.com>
2023-11-01 17:35:51 +0800

    1101_1731

--- hw_1/client.py
+++ hw_1/client.py
@@ -3,18 +3,25 @@ from torch.utils.data import TensorDataset
 from torch.utils.data import DataLoader
 from load import *
 
+'''
+datasetBalanceAllocation分到了ClientGroup类外面，用法见main.py
+client类被字典CG.clients_set索引
+client.num_example不知道是干什么的，照抄过来了
+'''
+
+def datasetBalanceAllocation(class_num, data):
+    clients_set = {}
+    for i in range(class_num):
+        feature, label = getdata(data, i, class_num)
+        someone = client(TensorDataset(torch.tensor(feature, dtype=torch.float, requires_grad=True), torch.tensor(label, dtype=torch.float, requires_grad=True)), torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), 20)
+        clients_set['client{}'.format(i)] = someone
+    return clients_set
+
 class ClientsGroup(object):
-    def __init__(self,dev,class_num,data):
+    def __init__(self,dev,class_num):
         self.dev=dev
         self.clients_set={}
         self.class_num=class_num
-        self.datasetBalanceAllocation(data)
-
-    def datasetBalanceAllocation(self,data):
-        for i in range(self.class_num):
-            feature,label=getdata(data,i)
-            someone=client(TensorDataset(torch.tensor(feature,dtype=torch.float,requires_grad=True),torch.tensor(label,dtype=torch.float,requires_grad=True)),torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),20)
-            self.clients_set['client{}'.format(i)]=someone
 
 class client(object):
     def __init__(self,trainDataSet,dev,num_example):

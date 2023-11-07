bbda08f2d49d6e79c275c36680e8af32e80ef461
n0n3nt1ty6857 <1043954252@qq.com>
2023-11-07 12:16:46 +0800

    fixed label and weightlist

--- hw_1/load.py
+++ hw_1/load.py
@@ -14,13 +14,21 @@ getdata(data,i,class_num):输入参数data为testfile的输出,class_num为客�
 def testfile():
     filename = "./test.csv"
     data = []
-    replacedict = {"Sun": [1, 0, 0, 0, 0, 0, 0], "Sat": [0, 1, 0, 0, 0, 0, 0], "Fri": [0, 0, 1, 0, 0, 0, 0], "Thurs": [0, 0, 0, 1, 0, 0, 0], "Wed": [0, 0, 0, 0, 1, 0, 0], "Tues": [0, 0, 0, 0, 0, 1, 0], "Mon": [0, 0, 0, 0, 0, 0, 1]}
+    replacedict = {
+        "Sun": [1, 0, 0, 0, 0, 0, 0],
+        "Sat": [0, 1, 0, 0, 0, 0, 0],
+        "Fri": [0, 0, 1, 0, 0, 0, 0],
+        "Thurs": [0, 0, 0, 1, 0, 0, 0],
+        "Wed": [0, 0, 0, 0, 1, 0, 0],
+        "Tues": [0, 0, 0, 0, 0, 1, 0],
+        "Mon": [0, 0, 0, 0, 0, 0, 1],
+    }
     with open(filename) as csvfile:
-        csv_reader=csv.reader(csvfile)
+        csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             if row[3] in replacedict:
-                rownew = row + replacedict[row[3]]
-                del rownew[3]
+                rownew = replacedict[row[3]] + row
+                del rownew[10]
             else:
                 rownew = row
             data.append(rownew)
@@ -29,12 +37,21 @@ def testfile():
     random.shuffle(data_int)
     return data_int
 
+
 def trainfile():
     filename = "./train.csv"
     data = []
-    replacedict = {"Sun": [1, 0, 0, 0, 0, 0, 0], "Sat": [0, 1, 0, 0, 0, 0, 0], "Fri": [0, 0, 1, 0, 0, 0, 0], "Thurs": [0, 0, 0, 1, 0, 0, 0], "Wed": [0, 0, 0, 0, 1, 0, 0], "Tues": [0, 0, 0, 0, 0, 1, 0], "Mon": [0, 0, 0, 0, 0, 0, 1]}
+    replacedict = {
+        "Sun": [1, 0, 0, 0, 0, 0, 0],
+        "Sat": [0, 1, 0, 0, 0, 0, 0],
+        "Fri": [0, 0, 1, 0, 0, 0, 0],
+        "Thurs": [0, 0, 0, 1, 0, 0, 0],
+        "Wed": [0, 0, 0, 0, 1, 0, 0],
+        "Tues": [0, 0, 0, 0, 0, 1, 0],
+        "Mon": [0, 0, 0, 0, 0, 0, 1],
+    }
     with open(filename) as csvfile:
-        csv_reader=csv.reader(csvfile)
+        csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             if row[3] in replacedict:
                 rownew = replacedict[row[3]] + row
@@ -52,15 +69,14 @@ def actualsplit(data):
     label = []
     feature = []
     for row in data:
-        label.append([row[-1]])
-        row.pop(-1)
+        label.append([row[13]])
+        row.pop(13)
         feature.append(row)
     return feature, label
 
 
 def getdata(data, i, weights):
-    data_new = data[int(sum(weights[:i])) : int(sum(weights[:i+1]))]
+    data_new = data[int(sum(weights[:i])) : int(sum(weights[: i + 1]))]
     feature, label = actualsplit(data_new)
     feature_std = preprocessing.StandardScaler().fit_transform(feature)
-    label_std = preprocessing.StandardScaler().fit_transform(label)
-    return feature_std, label_std
+    return feature_std, label
